eb26df1eb35d6450fdb9b799d14de4a3c7f8808f
okutani <okutani.nt@gmail.com>
2017-02-15 01:08:59 +0900

    remove returns 15 test

--- rspec_test/spec/calc_spec.rb
+++ rspec_test/spec/calc_spec.rb
@@ -4,5 +4,11 @@ RSpec.describe "A calc" do
   it "given 2 and 3, return 5" do
     calc = Calc.new
     expect(calc.add(2, 3)).to eq(5)
+  before do
+    @calc = Calc.new
+  end
+
+  it "given 2 and 3, returns 5" do
+    expect(@calc.add(2, 3)).to eq(5)
   end
 end
