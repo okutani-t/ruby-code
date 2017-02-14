25c9a224602cf18efef13c26e5141424bff1ab1e
okutani <okutani.nt@gmail.com>
2017-02-14 20:58:06 +0900

    add fake calc class

---
+++ rspec_test/lib/calc.rb
@@ -0,0 +1,5 @@
+class Calc
+  def add(a, b)
+    5
+  end
+end
--- rspec_test/spec/calc_spec.rb
+++ rspec_test/spec/calc_spec.rb
@@ -1,6 +1,8 @@
+require 'calc'
+
 RSpec.describe "A calc" do
   it "given 2 and 3, return 5" do
     calc = Calc.new
-    expect(calc.add(2, 3).to eq(5))
+    expect(calc.add(2, 3)).to eq(5)
   end
 end
