0c7801edbbb8f965bb790b07d01f8a98f6119734
okutani <okutani.nt@gmail.com>
2017-02-15 01:16:27 +0900

    add given 2 and 3 test

--- rspec_test/spec/calc_spec.rb
+++ rspec_test/spec/calc_spec.rb
@@ -7,5 +7,11 @@ RSpec.describe Calc do
 
   it "given 2 and 3, returns 5" do
     expect(@calc.add(2, 3)).to eq(5)
+    expect(@calc.add(2, 3)).not_to eq(8)
+    expect(@calc.add(2, 3)).to be < 10
+    expect(@calc.add(2, 3)).to be_between(1, 10).inclusive
+    expect(@calc).to respond_to(:add)
+    expect(@calc.add(2, 3).integer?).to be true
+    expect(@calc.add(2, 3)).to be_integer
   end
 end
