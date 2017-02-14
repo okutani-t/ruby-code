84b5ce9ff017b3f839925df1a012703419051e66
okutani <okutani.nt@gmail.com>
2017-02-15 01:36:21 +0900

    Change before def to subject

--- rspec_test/spec/calc_spec.rb
+++ rspec_test/spec/calc_spec.rb
@@ -1,17 +1,15 @@
 require 'calc'
 
 RSpec.describe Calc do
-  before do
-    @calc = Calc.new
-  end
+  subject(:calc) { Calc.new }
 
   it "given 2 and 3, returns 5" do
-    expect(@calc.add(2, 3)).to eq(5)
-    expect(@calc.add(2, 3)).not_to eq(8)
-    expect(@calc.add(2, 3)).to be < 10
-    expect(@calc.add(2, 3)).to be_between(1, 10).inclusive
-    expect(@calc).to respond_to(:add)
-    expect(@calc.add(2, 3).integer?).to be true
-    expect(@calc.add(2, 3)).to be_integer
+    expect(calc.add(2, 3)).to eq(5)
+    expect(calc.add(2, 3)).not_to eq(8)
+    expect(calc.add(2, 3)).to be < 10
+    expect(calc.add(2, 3)).to be_between(1, 10).inclusive
+    expect(calc).to respond_to(:add)
+    expect(calc.add(2, 3).integer?).to be true
+    expect(calc.add(2, 3)).to be_integer
   end
 end
