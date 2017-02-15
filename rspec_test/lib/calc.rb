cd195748eeef57fd0e0af5a6aedcd8f5045a2239
okutani <okutani.nt@gmail.com>
2017-02-16 05:33:46 +0900

    Add price function

--- rspec_test/lib/calc.rb
+++ rspec_test/lib/calc.rb
@@ -2,4 +2,7 @@ class Calc
   def add(a, b)
     a + b
   end
+  def price(x, tax)
+    x * (1 + tax)
+  end
 end
--- rspec_test/spec/calc_spec.rb
+++ rspec_test/spec/calc_spec.rb
@@ -3,6 +3,16 @@ require 'calc'
 RSpec.describe Calc do
   subject(:calc) { Calc.new }
 
+  context "tax 5%" do
+    let(:tax) { 0.05 }
+    it { expect(calc.price(100, tax)).to eq(105) }
+  end
+
+  context "tax 8%" do
+    let(:tax) { 0.08 }
+    it { expect(calc.price(100, tax)).to eq(108) }
+  end
+
   it "given 2 and 3, returns 5" do
     expect(calc.add(2, 3)).to eq(5)
     expect(calc.add(2, 3)).not_to eq(8)
